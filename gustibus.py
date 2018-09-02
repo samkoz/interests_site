ec453cc0e49cc8b57af0b44f09a5253c38d3c593
Sam Kozloff <samkozloff89@gmail.com>
2018-09-02 11:52:04 -0400

    initial testing added

--- gustibus.py
+++ gustibus.py
@@ -5,3 +5,12 @@ from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+
+# testing
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
